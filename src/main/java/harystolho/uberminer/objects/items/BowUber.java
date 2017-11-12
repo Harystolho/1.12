6b00e82fe3ccc36545ce6dc43202a67329e6a896
Harystolho <haristolhogamer@gmail.com>
2017-11-12 16:34:48 -0200

    added functionality

--- src/main/java/harystolho/uberminer/objects/items/BowUber.java
+++ src/main/java/harystolho/uberminer/objects/items/BowUber.java
@@ -3,15 +3,17 @@ package harystolho.uberminer.objects.items;
 import java.util.List;
 
 import javax.annotation.Nullable;
-import javax.management.MBeanTrustPermission;
 
 import harystolho.uberminer.Main;
 import harystolho.uberminer.init.ItemInit;
 import harystolho.uberminer.utils.IHasModel;
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -27,16 +29,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BowUber extends Item implements IHasModel {
 
-	int durability;
-	int radius;
-	int fortune;
-	int speed;
-
 	public BowUber(String name) {
-		durability = 100;
-		radius = 1;
-		fortune = 0;
-		speed = 1;
 		setUnlocalizedName(name);
 		setRegistryName(name);
 		setCreativeTab(Main.UBERMINER);
@@ -48,7 +41,7 @@ public class BowUber extends Item implements IHasModel {
 					return 0.0F;
 				} else {
 					return entityIn.getActiveItemStack().getItem() != ItemInit.TOOL_UBER ? 0.0F
-							: (float) (stack.getMaxItemUseDuration() - entityIn.getItemInUseCount()) / 20.0F * speed;
+							: (float) (stack.getMaxItemUseDuration() - entityIn.getItemInUseCount()) / 20.0F;
 				}
 			}
 		});
@@ -59,7 +52,7 @@ public class BowUber extends Item implements IHasModel {
 						: 0.0F;
 			}
 		});
-		
+
 		ItemInit.ITEMS.add(this);
 
 	}
@@ -67,16 +60,16 @@ public class BowUber extends Item implements IHasModel {
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
 		NBTTagCompound nbttagcompound = playerIn.getHeldItemMainhand().getTagCompound();
-		if(nbttagcompound == null) {
+		if (nbttagcompound == null) {
 			NBTTagCompound nbttagcompound1 = new NBTTagCompound();
-			nbttagcompound1.setString("ToolDurability", "100");
-			nbttagcompound1.setString("ToolRadius", "1");
-			nbttagcompound1.setString("ToolSpeed", "1");
-			nbttagcompound1.setString("ToolModifiers", "");
-			
+			nbttagcompound1.setInteger("ToolDurability", 100);
+			nbttagcompound1.setInteger("ToolRadius", 1);
+			nbttagcompound1.setInteger("ToolSpeed", 1);
+			nbttagcompound1.setString("ToolModifiers", "---");
+
 			playerIn.getHeldItemMainhand().setTagCompound(nbttagcompound1);
 		}
-		
+
 		ItemStack itemstack = playerIn.getHeldItem(handIn);
 		boolean flag = true;
 
@@ -98,27 +91,45 @@ public class BowUber extends Item implements IHasModel {
 	public void onPlayerStoppedUsing(ItemStack stack, World worldIn, EntityLivingBase entityLiving, int timeLeft) {
 
 		if ((stack.getMaxItemUseDuration() - entityLiving.getItemInUseCount()) / 20.0F > 5.0) {
+			NBTTagCompound tag = stack.getTagCompound();
+			int radius = tag.getInteger("ToolRadius");
 			for (int x = -radius; x < radius + 1; x++) {
 				for (int y = -radius; y < radius + 1; y++) {
 					for (int z = -radius; z < radius + 1; z++) {
 						BlockPos blockpos = new BlockPos(entityLiving.getPosition().getX() + x,
 								entityLiving.getPosition().getY() + y, entityLiving.getPosition().getZ() + z);
 						IBlockState iblockstate = worldIn.getBlockState(blockpos);
-						System.out.println(iblockstate.getBlock());
+						if (isOre(iblockstate.getBlock())) {
+							IBlockState stone = Blocks.STONE.getDefaultState();
+							worldIn.setBlockState(blockpos, stone);
+							ItemStack item = new ItemStack(iblockstate.getBlock());
+						
+							entityLiving.entityDropItem(item, 1F);
+						}
 					}
 				}
 			}
 		}
 	}
+	
+	/*	EntityItem t = new EntityItem(worldIn);
+	t.setItem(item);
+	t.setPosition(entityLiving.getPosition().getX(), entityLiving.getPosition().getY()+1, entityLiving.getPosition().getZ());
+	t.setPickupDelay(1);
+	worldIn.spawnEntity(t);*/
+
+	// @SideOnly(Side.SERVER)
+	public boolean isOre(Block block) {
+		if (block.getRegistryName().toString().toLowerCase().contains("ore")
+				|| block.getUnlocalizedName().toString().toLowerCase().contains("ore")) {
+			return true;
+		}
+		return false;
+	}
 
 	@Override
 	public int getMaxItemUseDuration(ItemStack stack) {
 		return 400;
-	}
-
-	public int getDurability() {
-
-		return durability;
 	}
 
 	public void UpdateNBT() {
@@ -129,14 +140,14 @@ public class BowUber extends Item implements IHasModel {
 	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
 		NBTTagCompound nbttagcompound = stack.getTagCompound();
 		if (nbttagcompound != null) {
-			NBTTagCompound nbttag_darability = nbttagcompound.getCompoundTag("ToolDurability");
-			NBTTagCompound nbttag_radius = nbttagcompound.getCompoundTag("ToolRadius");
-			NBTTagCompound nbttag_speed = nbttagcompound.getCompoundTag("ToolSpeed");
-			NBTTagCompound nbttag_modifier = nbttagcompound.getCompoundTag("ToolModifiers");
-			tooltip.add("Durability: " + nbttag_darability.toString());
-			tooltip.add("Radius: " + nbttag_radius.toString());
-			tooltip.add("Speed: " + nbttag_speed.toString());
-			tooltip.add("Modifiers: [" + nbttag_modifier.toString() + "]");
+			int nbttag_darability = nbttagcompound.getInteger("ToolDurability");
+			int nbttag_radius = nbttagcompound.getInteger("ToolRadius");
+			int nbttag_speed = nbttagcompound.getInteger("ToolSpeed");
+			String nbttag_modifier = nbttagcompound.getString("ToolModifiers");
+			tooltip.add("Durability: " + nbttag_darability);
+			tooltip.add("Radius: " + nbttag_radius);
+			tooltip.add("Speed: " + nbttag_speed);
+			tooltip.add("Modifiers: [" + nbttag_modifier + "]");
 		}
 	}
 
