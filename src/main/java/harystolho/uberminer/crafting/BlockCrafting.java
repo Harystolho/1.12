6d299c64dcf08ae1512e04dbb3c23fb517287c43
Harystolho <haristolhogamer@gmail.com>
2017-11-12 14:18:02 -0200

    Added new recipes

--- src/main/java/harystolho/uberminer/crafting/BlockCrafting.java
+++ src/main/java/harystolho/uberminer/crafting/BlockCrafting.java
@@ -4,14 +4,20 @@ import harystolho.uberminer.init.BlockInit;
 import harystolho.uberminer.utils.RecipeHelper;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class BlockCrafting {
 
 	public static void init() {
 		
 		RecipeHelper.addOldShaped(new ItemStack(BlockInit.BLOCK_UBER_CRAFTER), " A ", "ABA", " A ",
-				'A',	new ItemStack(Blocks.DIAMOND_BLOCK, 1),
+				'A',	new ItemStack(Blocks.DIAMOND_BLOCK),
 				'B',	new ItemStack(Blocks.EMERALD_BLOCK));
+		
+		RecipeHelper.addOldShaped(new ItemStack(BlockInit.BLOCK_UBER_TABLE), "AAA", "ABA", "AAA",
+				'A',	new ItemStack(Blocks.STONE),
+				'B',	"logWood");
+
 		
 	}
 	
--- src/main/java/harystolho/uberminer/crafting/ItemCrafting.java
+++ src/main/java/harystolho/uberminer/crafting/ItemCrafting.java
@@ -1,23 +1,17 @@
 package harystolho.uberminer.crafting;
 
-import harystolho.uberminer.init.BlockInit;
 import harystolho.uberminer.init.ItemInit;
 import harystolho.uberminer.utils.RecipeHelper;
-import harystolho.uberminer.utils.handlers.RegistryHandlers;
-import net.minecraft.client.Minecraft;
 import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.IRecipe;
 
 public class ItemCrafting {
 
 	public static void init() {
-
-		RecipeHelper.addOldShaped(new ItemStack(BlockInit.BLOCK_UBER_CRAFTER), " A ", "ABA", " A ",
-				'A',	new ItemStack(Blocks.DIAMOND_BLOCK, 1),
-				'B',	new ItemStack(Blocks.EMERALD_BLOCK));
-	
+		
+		RecipeHelper.addOldShaped(new ItemStack(ItemInit.TOOL_UBER), "   ", " B ", "   ",
+				'B',	new ItemStack(Blocks.DRAGON_EGG));
+		
 	}
 
 }
