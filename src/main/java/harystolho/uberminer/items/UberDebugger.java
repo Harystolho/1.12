97a96b2fa4f02bd208bb58a48f77a0abc281b918
Harystolho <haristolhogamer@gmail.com>
2017-11-20 21:12:17 -0200

    Minor Fixes

--- src/main/java/harystolho/uberminer/items/UberDebugger.java
+++ src/main/java/harystolho/uberminer/items/UberDebugger.java
@@ -3,16 +3,23 @@ package harystolho.uberminer.items;
 import harystolho.uberminer.Main;
 import harystolho.uberminer.init.ItemInit;
 import harystolho.uberminer.utils.IHasModel;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
-public class UberDebugger extends Item implements IHasModel{
+public class UberDebugger extends Item implements IHasModel {
 
 	public UberDebugger(String name) {
 		setUnlocalizedName(name);
 		setRegistryName(name);
 		setMaxStackSize(1);
 		setCreativeTab(Main.UBERMINER);
-		
+
 		ItemInit.ITEMS.add(this);
 	}
 
@@ -20,6 +27,17 @@ public class UberDebugger extends Item implements IHasModel{
 	public void registerModels() {
 		Main.proxy.registerItemRenderer(this, 0, "inventory");
 	}
-	
-	
+
+	public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand,
+			EnumFacing facing, float hitX, float hitY, float hitZ) {
+		if (!worldIn.isRemote) {
+			BlockPos pos2 = new BlockPos(hitX, hitY, hitZ);
+			IBlockState blockstate = worldIn.getBlockState(pos);
+			System.out.println(blockstate.getBlock().getLocalizedName());
+
+		}
+
+		return EnumActionResult.PASS;
+	}
+
 }
